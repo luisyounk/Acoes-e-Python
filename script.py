2345133bb55138383dd658d40ed2d3b02fde39b8
Luis Alexandre Dias dos Santos <luisyounk@gmail.com>
2023-02-10 17:29:33 -0300

    finalizado

--- script.py
+++ script.py
@@ -4,6 +4,11 @@ import yfinance as yf
 from matplotlib import pyplot as plt
 import mplcyberpunk
 import win32com.client as win32
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+import os
 
 codigos_de_negociacao = ["^BVSP", "BRL=X"]
 
@@ -57,6 +62,129 @@ retorno_anual_dolar = retorno_anual.iloc[-1, 0]
 retorno_anual_ibov = retorno_anual.iloc[-1, 1]
 
 
-print(retorno_anual)
-print(retorno_anual_dolar)
-print(retorno_anual_ibov)
+#print(retorno_anual)
+#print(retorno_anual_dolar)
+#print(retorno_anual_ibov)
+
+retorno_diario_dolar = round(retorno_diario_dolar * 100, 2)
+retorno_diario_ibov = round(retorno_diario_ibov * 100, 2)
+
+retorno_mensal_dolar = round(retorno_mensal_dolar * 100, 2)
+retorno_mensal_ibov = round(retorno_mensal_ibov * 100, 2)
+
+retorno_anual_dolar = round(retorno_anual_dolar * 100, 2)
+retorno_anual_ibov = round(retorno_anual_ibov * 100, 2)
+
+#print(retorno_diario_dolar)
+#print(retorno_diario_ibov)
+
+#FAZER OS GRAFICOS DA PERFORMANCE DO ULTIMO ANO DOS ATIVOS
+
+plt.style.use("cyberpunk")
+dados_fechamento.plot(y="ibovespa", use_index=True, legend=False)
+plt.title("ibovespa")
+plt.savefig('ibovespa.png', dpi=300)
+#plt.show()
+
+plt.style.use("cyberpunk")
+dados_fechamento.plot(y="dolar", use_index=True, legend=False)
+plt.title("dolar")
+plt.savefig('dolar.png', dpi=300)
+#plt.show()
+
+#ENVIAR EMAIL
+
+#outlook = win32.Dispatch('outlook.application')
+
+#email = outlook.CreateItem(0)
+
+#email.To = "luisyounk@gmail.com"
+#email.Subject = "Relatório Diário"
+#email.Body = f'''Prezado futuro chefe, segue o relatório diário:
+
+#Bolsa:
+
+#No anos o Ibovespa está tendo uma rentabilidade de {retorno_anual_ibov}%,
+#enquanto no mês a rentabilidade é de {retorno_mensal_ibov}%.
+
+#No último dia útil, o fechamento do Ibovespa foi de {retorno_diario_ibov}%.
+
+#Dólar:
+
+#No ano o Dólar está tendo uma rentabilidade de {retorno_anual_dolar}%,
+#enquanto no mês a rentabilidade é de {retorno_mensal_dolar}%.
+
+#No último dia útil, o fechamento do Dólar foi de {retorno_diario_dolar}%.
+
+#Abs,
+
+#Luis Alexandre Dias dos Santos, seu novo estágiario.
+
+#'''
+
+#anexo_ibovespa = r'C:\Users\luisy\OneDrive\Área de Trabalho\python-ações\A--es-e-Python\ibovespa.png'
+#anexo_dolar = r'C:\Users\luisy\OneDrive\Área de Trabalho\python-ações\A--es-e-Python\dolar.png'
+
+
+#email.Attachments.Add(anexo_ibovespa)
+#email.Attachments.Add(anexo_dolar)
+
+#email.Send()
+
+
+
+# Dados do remetente
+email_user = 'luisyounk@gmail.com'
+email_password = 'sua-senha'
+email_destinatario = 'luisyounk@hotmail.com'
+
+# Configurações do e-mail
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_destinatario
+msg['Subject'] = "Relatório Diário "
+
+# Corpo da mensagem
+body = f'''Prezado futuro chefe, segue o relatório diário:
+
+Bolsa:
+
+No anos o Ibovespa está tendo uma rentabilidade de {retorno_anual_ibov}%,
+enquanto no mês a rentabilidade é de {retorno_mensal_ibov}%.
+
+No último dia útil, o fechamento do Ibovespa foi de {retorno_diario_ibov}%.
+
+Dólar:
+
+No ano o Dólar está tendo uma rentabilidade de {retorno_anual_dolar}%,
+enquanto no mês a rentabilidade é de {retorno_mensal_dolar}%.
+
+No último dia útil, o fechamento do Dólar foi de {retorno_diario_dolar}%.
+
+Abs,
+
+Luis Alexandre Dias dos Santos, seu novo estágiario. 
+
+'''
+msg.attach(MIMEText(body,'plain'))
+
+# Anexos
+anexo_ibovespa = r'C:\Users\luisy\OneDrive\Área de Trabalho\python-ações\A--es-e-Python\ibovespa.png'
+anexo_dolar = r'C:\Users\luisy\OneDrive\Área de Trabalho\python-ações\A--es-e-Python\dolar.png'
+
+with open(anexo_ibovespa, 'rb') as f:
+    img_ibovespa = MIMEImage(f.read())
+    msg.attach(img_ibovespa)
+
+with open(anexo_dolar, 'rb') as f:
+    img_dolar = MIMEImage(f.read())
+    msg.attach(img_dolar)
+
+# Conexão com o servidor de e-mail
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email_user, email_password)
+text = msg.as_string()
+server.sendmail(email_user, email_destinatario, text)
+server.quit()
+
